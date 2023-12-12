f837a95fa727b57af51a4224d5f37fc9a650550f
CJMDJY <2077987156@qq.com>
2023-12-12 17:02:10 +0800

    servo control V2.0 :)

--- Servo_control.py
+++ Servo_control.py
@@ -4,10 +4,8 @@ import time
 from pca9685 import PCA9685
 import datetime
 
-
 pwm = PCA9685(0x40, debug=True)  # 对象初始化，指定IIC地址
 pwm.setPWMFreq(50)  # 设置PWM的频率
-
 
 
 class Servo:
@@ -15,19 +13,24 @@ class Servo:
         self.nextAngle = None
         self.cel = cel  # 通道设置
         self.deg = 0
-        self.time_stamp = datetime.datetime.now()
+        self.time_stamp = round(time.time(), 3)
         self.now_time = None
         print("舵机初始化时间" + str(self.time_stamp))
         pwm.set_servo_angle(cel, 0)  # 重置舵机
 
     def track(self, track_angle):
-        self.now_time = datetime.datetime.now()
-
-        pass
+        self.now_time = round(time.time(), 3)
+        if self.now_time - self.time_stamp >= 0.050:  # 时间检测
+            if track_angle - self.deg >= 2:  # 角度检测
+                return True
+            else:
+                return False
+        else:
+            return False
 
     def run(self, angle):
-        self.track(angle)
-        time.sleep(0.05)  # 休眠
+        if self.track(angle):
+            pwm.set_servo_angle(self.cel, angle)
 
     def information(self):
         print(self.deg)
