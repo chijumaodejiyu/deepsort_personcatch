7fe807b56047b91087661328773b85526103be8c
CJMDJY <2077987156@qq.com>
2023-12-13 12:09:47 +0800

    tmp

--- tmp_test.py
+++ tmp_test.py
@@ -1,4 +1,13 @@
-import datetime
+from time import sleep
+from Servo_control import *
 
-time = datetime.datetime.now()
+servo0 = Servo(3)
+timer = datetime.datetime.now()
 print(time)
+
+servo0.information()
+while True:
+    servo0.run(0)
+    time.sleep(2)
+    servo0.run(90)
+
