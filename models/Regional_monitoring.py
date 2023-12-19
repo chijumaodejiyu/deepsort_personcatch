58f24f0a55723e367c1c4c67e2a1d66b34d5fba1
CJMDJY <2077987156@qq.com>
2023-12-19 13:28:08 +0800

    'cjmdjy'

--- models/Regional_monitoring.py
+++ models/Regional_monitoring.py
@@ -1,11 +1,18 @@
 import numpy as np
+from PIL import Image
+
+
 class Regional_monitoring:
-    def __init__(self, region_input):
-        self.region = region_input
+    def __init__(self):
+        # self.region = region_input
         self.solid_coordinates = None
         self.track_id = -1
-        self.x1, self.y1, self.x2, self.y2 = None
+        self.image_dir = r"./models/region.png"
 
+    def pic_data_read(self):
+        png = Image.open(self.image_dir)
+        data = np.asarray(png)
+        print(data)
 
     def check(self, item_bbox: list):
         try:
@@ -13,14 +20,13 @@ class Regional_monitoring:
             xy1 = np.array([x1, y1])
             xy2 = np.array([x2, y2])
             xyc = (xy1 + xy2) / 2
-            self.solid_coordinates = np.mat(((self.x1+self.x2)/2), ((self.y1+self.y2)/2))
-
+            print(xyc)
+            # self.solid_coordinates = np.mat(((self.x1 + self.x2) / 2), ((self.y1 + self.y2) / 2))
 
         except:
             print('can not read item_bbox successfully')
             pass
 
 
-
-
 if __name__ == "__main__":
+    test = Regional_monitoring
