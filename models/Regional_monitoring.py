8cf78ee4b646b3bee537f43d41ac31af515c1adf
CJMDJY <2077987156@qq.com>
2023-12-20 12:34:51 +0800

    'cjmdjy'

--- models/Regional_monitoring.py
+++ models/Regional_monitoring.py
@@ -8,14 +8,13 @@ class Regional_monitoring:
         self.monitoring = None
         self.pic_data_read()
 
-    def pic_data_read(self):
-        data = cv2.imread(self.image_dir, cv2.IMREAD_GRAYSCALE)
-        # data = cv2.colorChange(data, cv2.COLOR_BGRA2GRAY)
-        self.monitoring = self.monitoring_process(data)
-        return data
-
     def monitoring_process(self, image: np.ndarray) -> np.ndarray:
         return np.where(image == 0, True, False)
+
+    def pic_data_read(self):
+        data = cv2.imread(self.image_dir, cv2.IMREAD_GRAYSCALE)
+        self.monitoring = self.monitoring_process(data)
+        return data
 
     def check(self, item_bbox: list):
         x1, y1, x2, y2, _, _ = item_bbox
@@ -24,6 +23,7 @@ class Regional_monitoring:
         xyc = (xy1 + xy2) // 2
         out = self.monitoring[xyc[1]][xyc[0]]
         return out
+
 
 if __name__ == "__main__":
     test = Regional_monitoring()
